d84907aa006ee4637f9606bfe643d9f14e3f1858
Dorisz <dorisz.albrecht@gmail.com>
2017-08-26 00:45:36 -0400

    Stochastic volatility module

--- src/StochasticVolatility.jl
+++ src/StochasticVolatility.jl
@@ -8,15 +8,11 @@ using StatsBase
 using StatPlots
 using Base.Test
 using ForwardDiff
-using ReverseDiff
 using Plots
 using ContinuousTransformations
-using ProfileView
 import ForwardDiff: Dual
-using BenchmarkTools
 using DiffWrappers
-import Distributions: Uniform, Normal
-Pkg.add("DiffWrappers")
+export StochasticVolatility
 plotlyjs()
 
 
@@ -45,7 +41,7 @@ end
 simulate_stochastic(ρ, σ, N) = simulate_stochastic(ρ, σ, rand(Chisq(1), N), randn(N))
 
 
-struct StochasticVolatility{T, Prior_ρ, Prior_σ}
+struct StochasticVolatility{T, Prior_ρ, Prior_σ, Ttrans}
     "observed data"
     ys::Vector{T}
     "prior for ρ (persistence)"
@@ -56,4 +52,99 @@ struct StochasticVolatility{T, Prior_ρ, Prior_σ}
     ϵ::Vector{T}
     "Normal(0,1) draws for simulation"
     ν::Vector{T}
+    "Transformations cached"
+    transformation::Ttrans
+end
+
+
+
+## In this form, we use an AR(2) process of the first differences with an intercept as the auxiliary model.
+
+"""
+    lag(xs, n, K)
+
+Lag-`n` operator on vector `xs` (maximum `K` lags).
+"""
+lag(xs, n, K) = xs[((K+1):end)-n]
+
+"""
+    lag_matrix(xs, ns, K = maximum(ns))
+
+Matrix with differently lagged xs.
+"""
+function lag_matrix(xs, ns, K = maximum(ns))
+    M = Matrix{eltype(xs)}(length(xs)-K, maximum(ns))
+    for i ∈ ns
+        M[:, i] = lag(xs, i, K)
     end
+    M
+end
+
+
+"first auxiliary regression y, X, meant to capture first differences"
+function yX1(zs, K)
+    Δs = diff(zs)
+    lag(Δs, 0, K), hcat(lag_matrix(Δs, 1:K, K), ones(eltype(zs), length(Δs)-K), lag(zs, 1, K+1))
+end
+
+"second auxiliary regression y, X, meant to capture levels"
+function yX2(zs, K)
+    lag(zs, 0, K), hcat(ones(eltype(zs), length(zs)-K), lag_matrix(zs, 1:K, K))
+end
+
+"Constructor which calculates cached values and buffers. Use this unless you know what you are doing."
+
+bridge_trans(dist::Uniform) = bridge(ℝ, Segment(dist.a, dist.b))
+bridge_trans(dist::InverseGamma) = bridge(ℝ, ℝ⁺)
+
+
+"""
+    Toy_Vol_Problem(ys, prior_ρ, prior_σ, M)
+
+Convenience constructor for Toy_Vol_Problem.
+Take in the observed data, the priors, and number of simulations (M).
+"""
+function StochasticVolatility(ys, prior_ρ, prior_σ, M)
+    StochasticVolatility(ys, prior_ρ, prior_σ, rand(Chisq(1), M), randn(M))
+end
+
+function StochasticVolatility(ys, prior_ρ, prior_σ, ϵ, ν)
+    transformation = TransformationTuple((bridge_trans(prior_ρ), (bridge_trans(prior_σ))))
+    StochasticVolatility(ys, prior_ρ, prior_σ, ϵ, ν, transformation)
+end
+
+
+function (pp::StochasticVolatility)(θ)
+    @unpack ys, prior_ρ, prior_σ, ν, ϵ, transformation = pp
+    ρ, σ = transformation(θ)
+    logprior = logpdf(prior_ρ, ρ) + logpdf(prior_σ, σ)
+    N = length(ϵ)
+
+    # Generating xs, which is the latent volatility process
+
+    xs = simulate_stochastic(ρ, σ, ϵ, ν)
+    Y_1, X_1 = yX1(xs, 2)
+    β₁ = qrfact(X_1, Val{true}) \ Y_1
+    v₁ = mean(abs2,  Y_1 - X_1*β₁)
+    Y_2, X_2 = yX2(xs, 2)
+    β₂ = qrfact(X_2, Val{true}) \ Y_2
+    v₂ = mean(abs2,  Y_2 - X_2*β₂)
+    # We work with first differences
+    y₁, X₁ = yX1(ys, 2)
+    log_likelihood1 = sum(logpdf.(Normal(0, √v₁), y₁ - X₁ * β₁))
+    y₂, X₂ = yX2(ys, 2)
+    log_likelihood2 = sum(logpdf.(Normal(0, √v₂), y₂ - X₂ * β₂))
+    logprior + log_likelihood1 + log_likelihood2
+end
+
+"""
+
+    path(parts...)
+
+`parts...` appended to the directory containing `src/` etc. Useful for saving graphs and data. *NOT EXPORTED*.
+
+"""
+
+path(parts...) = normpath(joinpath(splitdir(Base.find_in_path("StochasticVolatility"))[1], "..", parts...))
+
+end
